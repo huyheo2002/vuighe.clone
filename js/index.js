4ffbbe62219c29a436319bd04dd3c9dfd3920dd5
longfn <longpn.golsoftvn@gmail.com>
2022-07-24 10:08:14 +0700

    Add function change theme

---
+++ js/index.js
@@ -0,0 +1,49 @@
+class ThemeController {
+  constructor() 
+  {
+    this.isDark = false;
+  }
+  changeTheme() {
+    if (this.isDark) {
+      this.goLight();
+      this.isDark = false;
+    } else {
+      this.goDark();
+      this.isDark = true;
+    }
+  }
+  goLight() {
+    // classname to be changed
+    let textColor = this.getTextColorComponent();
+    textColor.forEach((component) => {
+      component.style.removeProperty('color');
+      // Chưa viết class cho cái này nên để sau
+      // component.classList.add('colorText-black');
+    });
+    let body = this.getBody(); 
+    body.classList.remove('bg-dark');
+    body.classList.add('bg-light');
+  }
+  goDark() {
+    let textColor = this.getTextColorComponent();
+    textColor.forEach((component) => {
+      component.style.color = '#1ab394';
+      // Chưa viết class cho cái này nên để sau
+      // component.classList.remove('colorText-black');
+    });
+    let body = this.getBody();
+    body.classList.remove('bg-light');
+    body.classList.add('bg-dark');
+  }
+  getTextColorComponent() {
+    return document.querySelectorAll('.colorText-black');
+  }
+  getBody() {
+    return document.querySelector('body'); 
+  }
+}
+
+let themeControler = new ThemeController();
+let userThemebtn = document.querySelector('#user-theme');
+userThemebtn.addEventListener("click", () => themeControler.changeTheme());
+console.log(userThemebtn);
